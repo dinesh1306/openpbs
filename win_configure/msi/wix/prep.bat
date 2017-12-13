7cde8a5521ce06d6dee2392d55de463325851dbf
Dinesh <dineshjoshi1306@gmail.com>
2017-12-13 18:49:42 +0530

    PBS-18115: Fix PP-1174 Update License text as per latest updates from Legal

--- win_configure/msi/wix/prep.bat
+++ win_configure/msi/wix/prep.bat
@@ -1,38 +1,39 @@
 @echo off
 
-REM Copyright (C) 1994-2017 Altair Engineering, Inc.
-REM For more information, contact Altair at www.altair.com.
-REM  
-REM This file is part of the PBS Professional ("PBS Pro") software.
-REM 
-REM Open Source License Information:
-REM  
-REM PBS Pro is free software. You can redistribute it and/or modify it under the
-REM terms of the GNU Affero General Public License as published by the Free 
-REM Software Foundation, either version 3 of the License, or (at your option) any 
-REM later version.
-REM  
-REM PBS Pro is distributed in the hope that it will be useful, but WITHOUT ANY 
-REM WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-REM PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
-REM  
-REM You should have received a copy of the GNU Affero General Public License along 
-REM with this program.  If not, see <http://www.gnu.org/licenses/>.
-REM  
-REM Commercial License Information: 
-REM 
-REM The PBS Pro software is licensed under the terms of the GNU Affero General 
-REM Public License agreement ("AGPL"), except where a separate commercial license 
-REM agreement for PBS Pro version 14 or later has been executed in writing with Altair.
-REM  
-REM Altair’s dual-license business model allows companies, individuals, and 
-REM organizations to create proprietary derivative works of PBS Pro and distribute 
-REM them - whether embedded or bundled with other software - under a commercial 
-REM license agreement.
-REM 
-REM Use of Altair’s trademarks, including but not limited to "PBS™", 
-REM "PBS Professional®", and "PBS Pro™" and Altair’s logos is subject to Altair's 
-REM trademark licensing policies.
+ Copyright (C) 1994-2018 Altair Engineering, Inc.
+ For more information, contact Altair at www.altair.com.
+
+ This file is part of the PBS Professional("PBS Pro") software.
+
+ Open Source License Information:
+
+ PBS Pro is free software. You can redistribute it and/or modify it under the
+ terms of the GNU Affero General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option) any
+ later version.
+
+ PBS Pro is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.
+ See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ Commercial License Information:
+
+ For a copy of the commercial license terms and conditions,
+ go to: (http://www.pbspro.com/UserArea/agreement.html)
+ or contact the Altair Legal Department.
+
+ Altair’s dual-license business model allows companies, individuals, and
+ organizations to create proprietary derivative works of PBS Pro and
+ distribute them - whether embedded or bundled with other software -
+ under a commercial license agreement.
+
+ Use of Altair’s trademarks, including but not limited to "PBS™",
+ "PBS Professional®", and "PBS Pro™" and Altair’s logos is subject to Altair's
+ trademark licensing policies.
 
 REM This script will generate the componentgroup directory structure
 
