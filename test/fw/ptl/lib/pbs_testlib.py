dc2e4b09c098b4e23300abb9684aa19dfa7e0040
Dinesh Joshi <dineshjoshi1306@gmail.com>
2020-08-27 12:59:11 +0530

    fix status issue

--- test/fw/ptl/lib/pbs_testlib.py
+++ test/fw/ptl/lib/pbs_testlib.py
@@ -5833,6 +5833,7 @@ class Server(PBSService):
                             return ret
                         else:
                             return [h.attributes for h in o.values()]
+                return []        
             else:
                 self.logger.error(self.logprefix + "unrecognized object type")
                 raise PbsStatusError(rc=-1, rv=[],
